94605ac910923de3e320ec77662054eee5df3207
bchiyanike <bchiyanike@gmail.com>
2026-05-02 18:06:43 +0200

    Update GameViewModel.kt

--- app/src/main/java/com/lionico/draft/ui/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/lionico/draft/ui/viewmodel/GameViewModel.kt
@@ -32,6 +32,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.random.Random
