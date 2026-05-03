71032adec1b1b2fd6fd48e339e9f0e84ff712e5a
bchiyanike <bchiyanike@gmail.com>
2026-05-03 13:18:10 +0200

    Update HistoryViewModel.kt

--- app/src/main/java/com/lionico/draft/ui/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/com/lionico/draft/ui/viewmodel/HistoryViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lionico.draft.data.model.GameResult
 import com.lionico.draft.data.repository.GameHistoryRepository
+import com.lionico.draft.domain.QuoteManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -13,13 +14,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HistoryViewModel @Inject constructor(
-    private val repository: GameHistoryRepository
+    private val repository: GameHistoryRepository,
+    quoteManager: QuoteManager
 ) : ViewModel() {
-    
+
     val history: StateFlow<List<GameResult>> = repository.getAllResults()
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = emptyList()
         )
+
+    val currentQuote: StateFlow<String> = quoteManager.currentQuote
 }
